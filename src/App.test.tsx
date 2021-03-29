33ac2ae690bcf60a2b215af676f11b5941db8c34
Vasiliy Gevko <v.gevko@bmlabs.org>
2021-03-30 00:11:34 +0300

    prettier fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(
@@ -12,7 +12,7 @@ test('renders learn react link', () => {
         <App />
       </Router>
     </Provider>
-  );
+  )
 
-  expect(getByText("Who wants to be a millionaire?")).toBeInTheDocument();
-});
+  expect(getByText('Who wants to be a millionaire?')).toBeInTheDocument()
+})
